b08635e74ac76c855d4b953cfa9100fc85414be1
Mark1096 <marco.rac1096@gmail.com>
2020-07-07 16:12:09 +0200

    fix quit player and css registration form

--- main.js
+++ main.js
@@ -330,18 +330,34 @@ function eventMessage(data) {
       break;
     }
     case 'exitPlayer': {
-      var room = endGame(data);
-      sendMessage({event: 'redirect', nickname: room.nickname1, data: {path: '/finishGame.html'}});
-      sendMessage({event: 'redirect', nickname: room.nickname2, data: {path: '/finishGame.html'}});
-      break;
+      var room = findRoomWithNickname(data.nickname);
+      sendMessage({event: 'stopGame', nickname: room.nickname1});
+      sendMessage({event: 'stopGame', nickname: room.nickname2});
+      delayEndGame(exitPlayer, data);
     }
     case 'quitPlayer': {
-      var room = endGame(data);
-      room.countReport++;
-      sendMessage({event: 'redirect', nickname: room.winner, data: {path: '/finishGame.html'}});
-      break;
+      var room = findRoomWithNickname(data.nickname);
+      sendMessage({event: 'stopGame', nickname: room.nickname1});
+      sendMessage({event: 'stopGame', nickname: room.nickname2});
+      delayEndGame(quitPlayer, data)
     }
   }
+}
+
+function delayEndGame(func, data) {
+  setTimeout(() => { func(data); }, 2000);
+}
+
+function exitPlayer(data) {
+  var room = endGame(data);
+  sendMessage({event: 'redirect', nickname: room.nickname1, data: {path: '/finishGame.html'}});
+  sendMessage({event: 'redirect', nickname: room.nickname2, data: {path: '/finishGame.html'}});
+}
+
+function quitPlayer(data) {
+  var room = endGame(data);
+  room.countReport++;
+  sendMessage({event: 'redirect', nickname: room.winner, data: {path: '/finishGame.html'}});
 }
 
 function endGame(data) {
@@ -609,6 +625,10 @@ function sendMessage(data) {
         case 'notFoundRoom': {
             socketClient.emit(data.event);
             break;
+        }
+        case 'stopGame': {
+          socketClient.emit(data.event);
+          break;
         }
       }
     }
